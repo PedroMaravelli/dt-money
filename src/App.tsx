f3a2a549f0f4fe6dd02fb803d5662ce49cc88a9d
PedroMaravelli <phmaravelli123@gmail.com>
2023-06-29 18:02:05 -0300

    creating the project structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/DefaultTheme";
+import { GloabalStyle } from "./styles/global";
+import Transaction from "./pages/Transactions";
+
+export function App() {
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GloabalStyle/>
+      <Transaction/>
+
+
+    </ThemeProvider>
+    
+  )
+}
+
+
